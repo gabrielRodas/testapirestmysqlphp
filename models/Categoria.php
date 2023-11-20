ee41eb53e0e7da6533217fb8c51274bf97cb8062
gabrielRodas <galejandrorodas@gmail.com>
2023-11-20 13:15:55 -0600

    20/11/21 Subiendo a Github

---
+++ models/Categoria.php
@@ -0,0 +1,64 @@
+<?php
+    class Categoria extends Conectar{
+        public function get_categoria(){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM categoria WHERE estado=1";
+            $sql=$conectar->prepare($sql);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function get_categoria_x_id($cat_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="SELECT * FROM categoria WHERE estado=1 AND cat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function insert_categoria($cat_nom, $cat_obs){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="INSERT INTO categoria(cat_id,cat_nom,cat_obs,estado) VALUES (NULL,?,?,'1');";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_nom);
+            $sql->bindValue(2, $cat_obs);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function update_categoria($cat_id, $cat_nom, $cat_obs){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE categoria set
+                cat_nom = ?,
+                cat_obs = ?
+                WHERE
+                cat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_nom);
+            $sql->bindValue(2, $cat_obs);
+            $sql->bindValue(3, $cat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+        public function delete_categoria($cat_id){
+            $conectar= parent::conexion();
+            parent::set_names();
+            $sql="UPDATE categoria set
+                estado = '0'
+                WHERE
+                cat_id = ?";
+            $sql=$conectar->prepare($sql);
+            $sql->bindValue(1, $cat_id);
+            $sql->execute();
+            return $resultado=$sql->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+    }
+
+?>
